8ab0159e20333b355a302ccdf01c1adab6c2d6be
tahakhandev123 <70109651+tahakhandev123@users.noreply.github.com>
2024-02-16 23:28:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const atm = 20000 ;
+switch (atm) {
+    case 40000:
+        document.write('you can  withdraw the money')
+        
+        break;
+
+        case 30000:
+            document.write('you can  withdraw the money')
+            
+            break;
+            case 20000:
+                document.write('you can  withdraw the money')
+                
+                break;
+                case 10000:
+                    document.write('you can  withdraw the money')
+                    
+                    break;
+                    case 50000:
+                        document.write('THE LIMIT DOES NOT MATCH')
+                        
+                        break;
+
+    default:
+        document.write('please withdraw value in limit')
+        break;
+}
